94c331cab7b54bc63951da41187baf348c9c2207
Vladimir Anghelov <jdms4gx6hr@privaterelay.appleid.com>
2026-04-03 15:26:56 +0300

    $$

--- master.c
+++ master.c
@@ -555,7 +555,6 @@ struct Student* loadFromFile(const char *filename, int *n) {
     return students;
 }
 int main() {
-    printf("Running master");
     int n;
     printf("How many students to add?: ");
     scanf("%d" , &n);
